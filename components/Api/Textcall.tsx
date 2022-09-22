a3ecfa0b5baf6214556b72b5b876ead06bdc87f6
LIRIN\Hime <ldaepoo1@gmail.com>
2022-09-22 17:19:44 +0700

    commit1a

--- components/Api/Textcall.tsx
+++ components/Api/Textcall.tsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { View, FlatList ,Layout, Text, Button} from "react-native";
 
-
-export function CallApi({ navigation }) {
+export function CallApi({}) {
   const [isLoading, setLoading] = useState(true); // set loading to true
   const [data, setData] = useState([]);
   useEffect(() => {
@@ -13,28 +11,4 @@ export function CallApi({ navigation }) {
       .finally(() => setLoading(false));
   }, []);
   console.log(data);
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      {isLoading ? 
-        <Text>Loading ...</Text> : (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <FlatList
-          data={data}
-          keyExtractor={({ id }, index) => id}
-          
-          renderItem={({ item }) => (
-            <Text category="h1" style={{ fontSize: 24 ,color: "black" }}>
-              Nmae: {item.name}
-              {"\n"}
-              Phone: {item.phone}
-              {"\n"}
-              {/*  */}
-            </Text>
-          )}
-        /> 
-        </View>
-      )}
-      
-    </View>
-  );
 }
--- components/Api/testAxios.tsx
+++ components/Api/testAxios.tsx
@@ -1,21 +1,9 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import { View, FlatList, Text, Button } from "react-native";
-import Constants from "expo-constants";
 
 export function CallApiAxiso({}) {
-  const [isLoading, setLoading] = useState(true); // set loading to true
-  const [data, setData] = useState([]);
   const baseUrl = "https://api2.plawarn.com";
   axios.get(`${baseUrl}/users`).then((res) => {
     // console.log(res.data);
   });
-  
-  return (
-    <View
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-
-    >
-    </View>
-  );
 }
